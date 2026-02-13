0c5fb99faef6a89a3f3d3b0eedd6a3e34774a3d0
Hemanth Ponugothi <phemanth8888@gmail.com>
2026-02-13 23:29:24 +0100

    Terraform

--- count/r53.tf
+++ count/r53.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_record" "roboshop" {
   count = length(var.instance)
   zone_id = var.zone_id
-  name    = "${var.instance[count.index]}.${var.domain_name}" # mongodb.daws86s.fun
+  name    = var.instance[count.index] == "frontend" ? var.domain_name : "${var.instance[count.index]}.${var.domain_name}"  
   type    = "A"
   ttl     = 1
-  records = [aws_instance.terraform[count.index].private_ip]
+  records = [var.instance[count.index] == "frontend" ? aws_instance.terraform[count.index].public_ip : aws_instance.terraform[count.index].private_ip]
   allow_overwrite = true
 }
