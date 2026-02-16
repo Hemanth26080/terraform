de8f8fb5e75a22e170ac2d08e28b8f757b8e4eed
Hemanth Ponugothi <phemanth8888@gmail.com>
2026-02-16 13:16:03 +0100

    terrafom

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,30 +1,30 @@
-# resource "aws_instance" "terraform" {
-#   for_each               = toset(var.instance)
-#   ami                    = data.aws_ami.Devops_ami.id
-#   instance_type          = "t3.micro"
-#   vpc_security_group_ids = [aws_security_group.Terraform-sg.id]
-#   tags = {
-#     Name        = each.key
-#     terraform   = "true"
-#     environment = "dev"
-#     project     = "terraform"
-#   }
+resource "aws_instance" "terraform" {
+  for_each               = toset(var.instance)
+  ami                    = data.aws_ami.Devops_ami.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.Terraform-sg.id]
+  tags = {
+    Name        = each.key
+    terraform   = "true"
+    environment = "dev"
+    project     = "terraform"
+  }
 
-# }
+}
 
-# resource "aws_security_group" "Terraform-sg" {
-#   name        = "Terraform-sg"
-#   description = "Created by terraform"
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_security_group" "Terraform-sg" {
+  name        = "terraform-practice-sg"
+  description = "Created by terraform"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- data_source/variable.tf
+++ data_source/variable.tf
@@ -1,15 +1,15 @@
-# variable "instance" {
-#     default = (["mongodb" , "redis" , "mysql" , "rabbitmq"])
-#     # default = ["mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend"]
-# }
+variable "instance" {
+    default = (["mongodb" , "redis" , "mysql" , "rabbitmq"])
+    # default = ["mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend"]
+}
 
-# variable "zone_id" {
-#     type = string
-#     default = "Z09571432H0XSZWLPQGY5"
-#     description = "Route53 Zone ID"
-# }
+variable "zone_id" {
+    type = string
+    default = "Z09571432H0XSZWLPQGY5"
+    description = "Route53 Zone ID"
+}
 
-# variable "domain_name" {
-#     default = "phemanth.in"
-#     description = "Domain Name"
-# }
+variable "domain_name" {
+    default = "phemanth.in"
+    description = "Domain Name"
+}
