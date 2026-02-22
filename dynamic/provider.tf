6b26c9a15ddbb4e61f94f42579d5d8865323ce04
Hemanth Ponugothi <phemanth8888@gmail.com>
2026-02-22 23:31:00 +0100

    terraform

---
+++ dynamic/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.32.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "terraform-s3-bucket-26"
+    key          = "terraform.dynamic.tfstate.demo"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
